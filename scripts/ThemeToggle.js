8ed3b7f30b6f6d550d5fc12755efff1c4f4f8944
Landay <67254560+landiehan@users.noreply.github.com>
2021-01-09 00:09:16 +0800

    add localStorage

--- scripts/ThemeToggle.js
+++ scripts/ThemeToggle.js
@@ -1,10 +1,17 @@
 const toggle = document.querySelector("#toggle");
+const currentTheme = localStorage.getItem("data-theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+}
+
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("data-theme", "light");
   }
 }
 toggle.addEventListener("change", switchTheme);
-
